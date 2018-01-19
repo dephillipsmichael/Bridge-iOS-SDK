5bfa42ed05a04fa66ec92085c4ccaf3955063975
Josh Bruhin <josh@saviapps.com>
2018-01-19 12:16:37 -0700

    RP-108 Implement secure coding so consents can be stored in UserDefaults

--- BridgeSDK/BridgeAPI/BridgeAPITypes/SBBConsentStatus.m
+++ BridgeSDK/BridgeAPI/BridgeAPITypes/SBBConsentStatus.m
@@ -35,4 +35,74 @@
 
 // Custom logic goes here.
 
+#pragma mark - NSCoding
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        self.consented = [aDecoder decodeObjectOfClass:[NSNumber class] forKey:NSStringFromSelector(@selector(consented))];
+        self.consentedValue = [[aDecoder decodeObjectOfClass:[NSNumber class] forKey:NSStringFromSelector(@selector(consentedValue))] boolValue];
+        self.name = [aDecoder decodeObjectOfClass:[NSString class] forKey:NSStringFromSelector(@selector(name))];
+        self.required = [aDecoder decodeObjectOfClass:[NSNumber class] forKey:NSStringFromSelector(@selector(required))];
+        self.requiredValue = [[aDecoder decodeObjectOfClass:[NSNumber class] forKey:NSStringFromSelector(@selector(requiredValue))] boolValue];
+        self.signedMostRecentConsent = [aDecoder decodeObjectOfClass:[NSNumber class] forKey:NSStringFromSelector(@selector(signedMostRecentConsent))];
+        self.signedMostRecentConsentValue = [[aDecoder decodeObjectOfClass:[NSNumber class] forKey:NSStringFromSelector(@selector(signedMostRecentConsentValue))] boolValue];
+        self.subpopulationGuid = [aDecoder decodeObjectOfClass:[NSString class] forKey:NSStringFromSelector(@selector(subpopulationGuid))];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [super encodeWithCoder:aCoder];
+    [aCoder encodeObject:self.consented forKey:NSStringFromSelector(@selector(consented))];
+    [aCoder encodeObject:[NSNumber numberWithBool:self.consentedValue] forKey:NSStringFromSelector(@selector(consentedValue))];
+    [aCoder encodeObject:self.name forKey:NSStringFromSelector(@selector(name))];
+    [aCoder encodeObject:self.required forKey:NSStringFromSelector(@selector(required))];
+    [aCoder encodeObject:[NSNumber numberWithBool:self.requiredValue] forKey:NSStringFromSelector(@selector(requiredValue))];
+    [aCoder encodeObject:self.signedMostRecentConsent forKey:NSStringFromSelector(@selector(signedMostRecentConsent))];
+    [aCoder encodeObject:[NSNumber numberWithBool:self.signedMostRecentConsentValue] forKey:NSStringFromSelector(@selector(signedMostRecentConsentValue))];
+    [aCoder encodeObject:self.subpopulationGuid forKey:NSStringFromSelector(@selector(subpopulationGuid))];
+}
+
+#pragma mark - NSCopy
+
+- (id)copyWithZone:(NSZone *)zone {
+    SBBConsentStatus *copy = [super copyWithZone:zone];
+    copy.consented = self.consented;
+    copy.consentedValue = self.consentedValue;
+    copy.name = self.name;
+    copy.required = self.required;
+    copy.requiredValue = self.requiredValue;
+    copy.signedMostRecentConsent = self.signedMostRecentConsent;
+    copy.signedMostRecentConsentValue = self.signedMostRecentConsentValue;
+    copy.subpopulationGuid = self.subpopulationGuid;
+    return copy;
+}
+
+#pragma mark - Equality
+
+- (BOOL)isEqual:(id)object {
+    typeof(self) castObject = object;
+    return [super isEqual:object] &&
+    [self.consented isEqual:castObject.consented] &&
+    [[NSNumber numberWithBool:self.consentedValue] isEqual:[NSNumber numberWithBool:castObject.consentedValue]] &&
+    [self.name isEqual:castObject.name] &&
+    [self.required isEqual:castObject.required] &&
+    [[NSNumber numberWithBool:self.requiredValue] isEqual:[NSNumber numberWithBool:castObject.requiredValue]] &&
+    [self.signedMostRecentConsent isEqual:castObject.signedMostRecentConsent] &&
+    [[NSNumber numberWithBool:self.signedMostRecentConsentValue] isEqual:[NSNumber numberWithBool:castObject.signedMostRecentConsentValue]] &&
+    [self.subpopulationGuid isEqual:castObject.subpopulationGuid];
+}
+
+- (NSUInteger)hash {
+    return super.hash ^
+    self.consented.hash ^
+    [NSNumber numberWithBool:self.consentedValue].hash ^
+    self.name.hash ^
+    self.required.hash ^
+    [NSNumber numberWithBool:self.requiredValue].hash ^
+    self.signedMostRecentConsent.hash ^
+    [NSNumber numberWithBool:self.signedMostRecentConsentValue].hash ^
+    self.subpopulationGuid.hash;
+}
+
 @end
